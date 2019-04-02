076d434658c62df83d18958256fb9ba264c5b384
AGI-THB <AGI-THB@users.noreply.github.com>
2019-04-02 23:40:11 +0200

    Erster richtiger Commit

---
+++ app/src/main/java/de/thb/schaffoener/Todo.java
@@ -0,0 +1,15 @@
+package de.thb.schaffoener;
+
+/* Todo hat einen Namen (1)
+ * Todo hat Beschreibung (1)
+ * Todo zeigt an ob erledigt oder nicht (1)
+ * Todo zeigt an ob besondern wichtig (favourite) oder nicht (1)
+ * Todo hat Faelligkeitsdatum und Uhrzeit(1)
+ * Alle Informationsbestandteile sind aenderbar (2)
+ *
+ */
+
+public class Todo {
+
+
+}
